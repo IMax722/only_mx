83c07f37bbefbc039412162239904fb9b7e14fdf
IMax722 <satalk77@gmail.com>
2018-04-15 05:26:19 +0300

    More add's and fix on views

---
+++ database/migrations/2018_04_14_135044_create_remarks__table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRemarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('remarks_', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('problem');
+            $table->string('sender');
+            $table->string('check_num');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('remarks_');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/main', function () {
+    return view('main');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
